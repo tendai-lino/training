c72c62f2d06ada2c3cffc96d2c1e65364eabc8dc
Ubuntu <ubuntu@ip-172-31-71-251.af-south-1.compute.internal>
2022-03-23 21:16:57 +0000

    to investigate from this stage

--- tgw-route-table.tf
+++ tgw-route-table.tf
@@ -44,11 +44,11 @@ resource "aws_ec2_transit_gateway_route_table" "security-attach-rtb" {
 resource "aws_ec2_transit_gateway_route" "security-to-vpc1-route-tgw" {
   destination_cidr_block         = "10.0.0.0/16"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.security-attach.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.vpc1-attach-rtb.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.security-attach-rtb.id
 }
 
 resource "aws_ec2_transit_gateway_route" "security-to-vpc2-route-tgw" {
   destination_cidr_block         = "192.168.0.0/16"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.security-attach.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.vpc2-attach-rtb.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.security-attach-rtb.id
 }

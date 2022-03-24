ac7065afb5bb3a963067fed941e8db932b8412f9
Ubuntu <ubuntu@ip-172-31-69-204.af-south-1.compute.internal>
2022-03-24 15:36:20 +0000

    twg and fw subnets route tables

--- subnets-routes.tf
+++ subnets-routes.tf
@@ -22,14 +22,32 @@ resource "aws_default_route_table" "vpc1rtb" {
     Name = "vpc2-route-table"
   }
   }
-   resource "aws_default_route_table" "securityrtb" {
-  default_route_table_id = aws_vpc.main3.default_route_table_id
+ 
+ 
+ 
+ 
+  resource "aws_route_table" "sec1-tgw-sub-route" {
+  vpc_id = aws_vpc.main3.id
 
-  # route {
-  #   cidr_block = "10.0.1.0/24"
-  #   gateway_id = aws_internet_gateway.example.id
-  # }
-      tags = {
-    Name = "securityvpc-route-table"
+  route {
+    cidr_block = "0.0.0.0/0"
+    vpc_endpoint_id = aws_vpc_endpoint.security_vpc_glwb_endpoint.id
   }
+
+   tags = {
+    Name = "transit-gateway-Subnet-route-table"
   }
+}
+  
+  resource "aws_route_table" "sec2-fw-sub-route" {
+  vpc_id = aws_vpc.main3.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    transit_gateway_id = aws_ec2_transit_gateway.mytgw.id
+  }
+
+   tags = {
+    Name = "Firewall-Subnet-route-table"
+  }
+}

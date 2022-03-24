16afbea5598eaef3df0da8977f4a6beeed64daac
Ubuntu <ubuntu@ip-172-31-69-204.af-south-1.compute.internal>
2022-03-24 15:02:19 +0000

    resolve association errors

--- tgw-route-table-association.tf
+++ tgw-route-table-association.tf
@@ -4,7 +4,7 @@ resource "aws_ec2_transit_gateway_route_table_association" "vpc1-associate" {
 }
 
 resource "aws_ec2_transit_gateway_route_table_association" "vpc2-associate" {
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc1attach.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.vpc2attach.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.vpc2-attach-rtb.id
 }
 

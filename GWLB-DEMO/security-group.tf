cb731bc3a602ab8e1ed3810a7dfb4f3d0d7008b1
Ubuntu <ubuntu@ip-172-31-73-153.af-south-1.compute.internal>
2022-03-26 13:26:53 +0000

    add target group and security group

---
+++ GWLB-DEMO/security-group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main3.cidr_block]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
